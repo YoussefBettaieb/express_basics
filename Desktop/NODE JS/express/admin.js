df48c51321573595fc4afe9934add9cdda16a448
YoussefBettaieb <youssef.bettaieb@insat.ucar.tn>
2025-02-20 16:54:22 +0100

    some changes wit hexpress

--- Desktop/NODE JS/express/admin.js
+++ Desktop/NODE JS/express/admin.js
@@ -1,16 +1,24 @@
 const express = require("express");
 
+const bodyparser = require("body-parser");
+
 const path = require("path");
+
 const router = express.Router();
 
+const products = [];
+
+router.use(bodyparser.urlencoded({ extended: false }));
 router.get("/add-product", (req, res, next) => {
   //console.log("In my app middleware!");
   res.sendFile(path.join(__dirname, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
-  console.log(req.body);
+router.post("/add-product", (req, res, next) => {
+  products.push({
+    title: req.body.title,
+  });
   res.redirect("/");
 });
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- Desktop/NODE JS/express/app.js
+++ Desktop/NODE JS/express/app.js
@@ -3,13 +3,17 @@ const bodyparser = require("body-parser");
 const path = require("path");
 const app = express();
 
-const adminRoutes = require("./admin");
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./admin");
 const shopRoutes = require("./shop");
 
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(adminRoutes);
 app.use(shopRoutes);
+app.use(adminData.routes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
